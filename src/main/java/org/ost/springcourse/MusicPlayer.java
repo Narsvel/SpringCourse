5c7ea5012a40dad4ad6e93e563d14f0a660970b9
Vyacheslav Ostrovsky <narsvel@mail.ru>
2025-03-27 09:29:52 +0300

    Lesson 11 annotation @Qualifier for Dependency Injection

--- src/main/java/org/ost/springcourse/MusicPlayer.java
+++ src/main/java/org/ost/springcourse/MusicPlayer.java
@@ -1,39 +1,44 @@
 package org.ost.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
 
-//    private Music music;
-//
 //    @Autowired
-//    public MusicPlayer(Music music) { //будет ошибка т.к. несколько бинов подходит для Music (ClassicalMusic, RockMusic, RapMusic)
-//        this.music = music;
-//    }
+//    @Qualifier("classicMusic") //указываем какой bean надо внедрять в Music firstMusic
+    private Music firstMusic;  //через рефлексию spring может использовать @Autowired даже на приватном поле
+//    @Autowired
+//    @Qualifier("rockMusic")
+    private Music secondMusic;
 
-    private ClassicMusic classicMusic;  //через рефлексию spring может использовать @Autowired даже на приватном поле
-    private RockMusic rockMusic;
-
-    @Autowired
-    public MusicPlayer(ClassicMusic classicMusic, RockMusic rockMusic) {
-        this.classicMusic = classicMusic;
-        this.rockMusic = rockMusic;
+    @Autowired //в кострукторе @Qualifier указывается в скобках
+    public MusicPlayer(@Qualifier("classicMusic") Music firstMusic,
+                       @Qualifier("rockMusic") Music secondMusic) {
+        this.firstMusic = firstMusic;
+        this.secondMusic = secondMusic;
     }
 
 //    @Autowired
-//    public void setClassicMusic(ClassicMusic classicMusic) {
-//        this.classicMusic = classicMusic;
+//    @Qualifier("classicMusic")
+//    public void setFirstMusic(Music firstMusic) {
+//        this.firstMusic = firstMusic;
+//    }
+//    @Autowired
+//    @Qualifier("rockMusic")
+//    public void setSecondMusic(Music secondMusic) {
+//        this.secondMusic = secondMusic;
 //    }
 
     public void playMusic() {
-        System.out.println("Playing: " + classicMusic.getPlaySong());
-        System.out.println("Playing: " + rockMusic.getPlaySong());
+        System.out.println("Playing: " + firstMusic.getPlaySong());
+        System.out.println("Playing: " + secondMusic.getPlaySong());
     }
 
     public String toMusic() {
-        return "Playing: " + classicMusic.getPlaySong() + " and " + rockMusic.getPlaySong();
+        return "Playing: " + firstMusic.getPlaySong() + " and " + secondMusic.getPlaySong();
     }
 
 
